b9af9fd04be6eff707cc4b589f619fdbc124e312
user <user@DESKTOP-7G66QFG>
2020-01-01 00:56:55 +0900

    SelectJdbcTemplate 분리

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 import core.jdbc.ConnectionManager;
 import next.model.User;
 import support.JdbcTemplate;
+import support.SelectJdbcTemplate;
 
 public class UserDao {
 
@@ -24,12 +25,33 @@ public class UserDao {
 			}			
 		};
 		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-		template.insert(sql);
+		template.executeUpdate(sql);
     }
 	
 
     public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+		JdbcTemplate template = new JdbcTemplate() {
+			@Override
+			public void setParameter(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, user.getPassword());
+				pstmt.setString(2, user.getName());
+				pstmt.setString(3, user.getEmail());
+				pstmt.setString(4, user.getUserId());
+			}			
+		};
+		String sql = "UPDATE USERS set password = ?, name = ?, email= ? where userId = ?";
+		template.executeUpdate(sql);
+    }
+    
+    public void remove(User user) throws SQLException {
+		JdbcTemplate template = new JdbcTemplate() {
+			@Override
+			public void setParameter(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, user.getUserId());
+			}			
+		};
+		String sql = "DELETE FROM USERS  where userId = ?";
+		template.executeUpdate(sql);
     }
 
     public List<User> findAll() throws SQLException {
@@ -38,34 +60,26 @@ public class UserDao {
     }
 
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
+		SelectJdbcTemplate template = new SelectJdbcTemplate() {
 
-            rs = pstmt.executeQuery();
+			@Override
+			public void setParameters(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, userId);
+			}
 
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+			@Override
+			public User mapRow(ResultSet rs) throws SQLException {
+				User user = null;
+				if (rs.next()) {
+				    user = new User(rs.getString("userId"), 
+				    		rs.getString("password"), 
+				    		rs.getString("name"),
+				            rs.getString("email"));
+				}
+				return user;
+			}
+		};
+		String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+		return template.executeQuery(sql);
     }
 }
--- src/main/java/support/SelectJdbcTemplate.java
+++ src/main/java/support/SelectJdbcTemplate.java
@@ -2,33 +2,40 @@ package support;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import core.jdbc.ConnectionManager;
-import next.dao.UserDao;
 import next.model.User;
 
-public abstract class JdbcTemplate {
-	
-    public void executeUpdate(String sql) throws SQLException {
+public abstract class SelectJdbcTemplate {
+
+    public User executeQuery(String sql) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
         try {
             con = ConnectionManager.getConnection();
             pstmt = con.prepareStatement(sql);
-            setParameter(pstmt);
+            setParameters(pstmt);
 
-            pstmt.executeUpdate();
+            rs = pstmt.executeQuery();
+
+            return mapRow(rs);
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
             if (pstmt != null) {
                 pstmt.close();
             }
-
             if (con != null) {
                 con.close();
             }
         }
     }
+    
+    public abstract void setParameters(PreparedStatement pstmt) throws SQLException;
+    public abstract User mapRow(ResultSet rs) throws SQLException ;
 
-    public abstract void setParameter(PreparedStatement pstmt) throws SQLException ;
 }
