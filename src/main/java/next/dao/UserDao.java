2eaa1e70b78159ebd435d64f3b40a8cb548a3547
user <user@DESKTOP-7G66QFG>
2020-01-01 12:35:41 +0900

    SQLException을 런타임 익셉션으로 변환해서 처리하는 방법  - DataAccessException 추가

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -33,26 +33,26 @@ import support.RowMapper;
 
 public class UserDao {
 
-	public void insert(User user) throws SQLException {
+	public void insert(User user) {
 		JdbcTemplate template = new JdbcTemplate();
 		String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
 		template.executeUpdate(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 	
 
-    public void update(User user) throws SQLException {
+    public void update(User user) {
     	JdbcTemplate template = new JdbcTemplate();
 		String sql = "UPDATE USERS set password = ?, name = ?, email= ? where userId = ?";
 		template.executeUpdate(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
     
-    public void remove(User user) throws SQLException {
+    public void remove(User user) {
     	JdbcTemplate template = new JdbcTemplate();
 		String sql = "DELETE FROM USERS  where userId = ?";
 		template.executeUpdate(sql, user.getUserId());
     }
 
-    public User findByUserId(String userId) throws SQLException {
+    public User findByUserId(String userId) {
     	RowMapper<User> rm = new RowMapper<User>() {
 			@Override
 			public User mapRow(ResultSet rs) throws SQLException {
@@ -67,7 +67,7 @@ public class UserDao {
 		return template.executeQuery(sql, rm, userId);
     }
 
-    public List<User> findAll() throws SQLException {
+    public List<User> findAll() {
     	RowMapper<User> rm = new RowMapper<User>() {
 			@Override
 			public User mapRow(ResultSet rs) throws SQLException {
--- src/main/java/support/JdbcTemplate.java
+++ src/main/java/support/JdbcTemplate.java
@@ -12,86 +12,96 @@ import next.dao.UserDao;
 import next.model.User;
 
 public class JdbcTemplate {
-	
-    public void executeUpdate(String sql, PreparedStatementSetter pss) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            pss.setParameter(pstmt);
 
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
+	public void executeUpdate(String sql, PreparedStatementSetter pss) throws DataAccessException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pss.setParameter(pstmt);
 
-            if (con != null) {
-                con.close();
-            }
-        }
-    }
-    
-    public void executeUpdate(String sql, Object... parameters) throws SQLException {
-        executeUpdate(sql, createPrepareStatementSetter(parameters));
-    }
-        
-    public <T> T executeQuery(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws SQLException {
-        List<T> list = list(sql, rm, pss);
-        if(list.isEmpty()) {
-        	return null;
-        }
-        return list.get(0);
-    }
-    
-    public <T> T executeQuery(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
-    	return executeQuery(sql, rm, createPrepareStatementSetter(parameters));
-    }
-    
-    public <T> List<T> list(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            pstmt = con.prepareStatement(sql);
-            pss.setParameter(pstmt);
-            
-            rs = pstmt.executeQuery();
-            
-            List<T> list = new ArrayList<T>();
-            while (rs.next()) {
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		} finally {
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				throw new DataAccessException(e);
+			}
+		}
+	}
+
+	public void executeUpdate(String sql, Object... parameters) {
+		executeUpdate(sql, createPrepareStatementSetter(parameters));
+	}
+
+	public <T> T executeQuery(String sql, RowMapper<T> rm, PreparedStatementSetter pss) {
+		List<T> list = list(sql, rm, pss);
+		if (list.isEmpty()) {
+			return null;
+		}
+		return list.get(0);
+	}
+
+	public <T> T executeQuery(String sql, RowMapper<T> rm, Object... parameters) {
+		return executeQuery(sql, rm, createPrepareStatementSetter(parameters));
+	}
+
+	public <T> List<T> list(String sql, RowMapper<T> rm, PreparedStatementSetter pss) throws DataAccessException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pss.setParameter(pstmt);
+
+			rs = pstmt.executeQuery();
+
+			List<T> list = new ArrayList<T>();
+			while (rs.next()) {
 				list.add(rm.mapRow(rs));
 			}
-            return list;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
-    }
-    
-    public <T> List<T> list(String sql, RowMapper<T> rm, Object... parameters) throws SQLException {
-    	return list(sql, rm, createPrepareStatementSetter(parameters));
-    }
+			return list;
+		} catch (SQLException e) {
+			throw new DataAccessException(e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				throw new DataAccessException(e);
+			}
+		}
+	}
 
- 
+	public <T> List<T> list(String sql, RowMapper<T> rm, Object... parameters) {
+		return list(sql, rm, createPrepareStatementSetter(parameters));
+	}
 
 	private PreparedStatementSetter createPrepareStatementSetter(Object... parameters) {
 		return new PreparedStatementSetter() {
 			@Override
 			public void setParameter(PreparedStatement pstmt) throws SQLException {
-	            for (int i = 0; i < parameters.length; i++) {
-	            	pstmt.setObject(i+1, parameters[i]);
+				for (int i = 0; i < parameters.length; i++) {
+					pstmt.setObject(i + 1, parameters[i]);
 				}
 			}
-    	};
+		};
 	}
 }
